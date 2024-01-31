b0ab0727d55a24c47245370087eb9abdf509badd
NirajNode24 <155453297+NirajNode24@users.noreply.github.com>
2024-01-31 17:45:58 +0530

    AWS RDS

--- DB/config.js
+++ DB/config.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
 
-// module.exports = {
-//     host: process.env.DB_HOST || host,
-//     user: process.env.DB_USER || user,
-//     password: process.env.DB_PASSWORD || password,
-//     database: process.env.DB_DATABASE || database,
-//     dialect : process.env.DB_DIALECT || 'mysql'
-// };
-
 module.exports = {
     host: process.env.DB_HOST || host,
     user: process.env.DB_USER || user,
     password: process.env.DB_PASSWORD || password,
     database: process.env.DB_DATABASE || database,
-    dialect : process.env.DB_DIALECT || 'postgres'
+    dialect : process.env.DB_DIALECT || 'mysql'
 };
+
+// module.exports = {
+//     host: process.env.DB_HOST || host,
+//     user: process.env.DB_USER || user,
+//     password: process.env.DB_PASSWORD || password,
+//     database: process.env.DB_DATABASE || database,
+//     dialect : process.env.DB_DIALECT || 'postgres'
+// };
