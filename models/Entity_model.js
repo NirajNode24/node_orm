d80b42136a694b084acc6752e5b5cf93dc37f6ee
NirajNode24 <155453297+NirajNode24@users.noreply.github.com>
2024-02-05 18:20:30 +0530

    resume

--- models/Entity_model.js
+++ models/Entity_model.js
@@ -1,14 +1,58 @@
 module.exports = (sequelize, DataTypes) => {
 
 const Entity = sequelize.define('Entity', {
-  Data: {
-    type: DataTypes.TEXT,
-    allowNull: true
-  },
-  Name: {
-    type: DataTypes.STRING,
-    allowNull: true
-  }
+    EntityName: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    Email: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    Password: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    MobileNumber: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    Switch: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+    },
+    Date: {
+      type: DataTypes.DATEONLY,
+      allowNull: true,
+    },
+    Time: {
+      type: DataTypes.TIME,
+      allowNull: true,
+    },
+    Image: {
+      type: DataTypes.BLOB,
+      allowNull: true,
+    },
+    Radio: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    Range: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    Description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    AddMembers: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    AddMultipleMembers: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
   });
   // Entite.associate = (models) => {
   //   Entite.belongsToMany(models.User, { through: 'UserEntite' });
